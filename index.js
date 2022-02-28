d686647d3eeb496ac17093516e9ad3002206e014
floverde <fabrizio.loverde@inter.ikea.com>
2022-02-28 14:15:10 +0100

    Initial setup for using EdgeGrid

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const EdgeGrid = require('akamai-edgegrid');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  var eg = new EdgeGrid(core.getInput('client-token'),
+						core.getInput('client-secret'),
+						core.getInput('access-token'),
+						core.getInput('base-uri'));
+
+  console.log(`Edge Grid successfully instantiated!!!`);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
